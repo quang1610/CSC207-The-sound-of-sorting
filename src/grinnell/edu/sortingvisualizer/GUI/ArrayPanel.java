e8ed55af9cc0a35b08e1ef1f38506740fae32e7c
gnomeann <45517239+gnomeann@users.noreply.github.com>
2019-04-17 17:26:34 -0500

    added documentation to ArrayPanel

--- src/grinnell/edu/sortingvisualizer/GUI/ArrayPanel.java
+++ src/grinnell/edu/sortingvisualizer/GUI/ArrayPanel.java
@@ -24,8 +24,10 @@ public class ArrayPanel extends JPanel {
 
   @Override
   public void paintComponent(Graphics g) {
+    // clear the rectangle before painting
     g.clearRect(0, 0, this.getWidth(), this.getHeight());
     
+    // set up values
     int length = notes.getNotes().length;
     int xTemp = 0;
     int yTemp = 0;
@@ -33,14 +35,22 @@ public class ArrayPanel extends JPanel {
     int heightTemp = 0;
     int unitHeight = this.getHeight()/length; 
     Color color;
+    
+    // for the length of notes...
     for(int i = 0; i < length; i++) {
+      //...the x coordinate is at i * width...
       xTemp = i * width;
+      // ...heightTemp is the number at the index i of the NoteIndices notes...
       heightTemp = unitHeight * (notes.getNotes()[i] + 1);
+      // ...and the y coordinate is the total visualizer's height - our heightTemp value.
       yTemp = this.getHeight() - heightTemp;
-      
-      color= new Color(notes.getNotes()[i] * 225 / length, 150, 150);
+      // The color of the bar changes depending on the height.
+      color = new Color(notes.getNotes()[i] * 225 / length, 150, 150);
       g.setColor(color);
+      
+      // draw a filled rectangle
       g.fillRect(xTemp, yTemp, width, heightTemp);
+      // if the note is highlighted, outline the rectangle with yellow
       if(notes.isHighlighted(i)) {
         g.setColor(Color.YELLOW);
         g.drawRect(xTemp, yTemp, width, heightTemp);
